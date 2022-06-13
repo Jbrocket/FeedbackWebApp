3261b8fcbbd48ae0a8b6a89bb953a9af7241a063
Jonathan Brockett <Jbrockett747@gmail.com>
2022-06-12 17:29:41 -0700

    Making the app

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FeedbackItem() {
+  return (
+    <div className='card'>
+        <div className='num-display'>10</div>
+        <div className='text-display'>Your application sucks balls</div>
+    </div>
+  )
+}
+
+export default FeedbackItem
