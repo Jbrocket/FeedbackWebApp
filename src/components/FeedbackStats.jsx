d4c5738988ed3b1e08e7d8892a0fd74564fa22ec
Jonathan Brockett <Jbrockett747@gmail.com>
2022-07-06 14:16:04 -0700

    Fixed bug where propType was None

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -17,7 +17,7 @@ function FeedbackStats({ feedback } ) {
 }
 
 FeedbackStats.propTypes = {
-    feeedback: PropTypes.array.isRequired
+    feedback: PropTypes.array.isRequired
 }
 
 export default FeedbackStats
