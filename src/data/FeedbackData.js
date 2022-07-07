41e87c1d6f207a7fcc369a8588de77ce3a82e2c4
Jonathan Brockett <Jbrockett747@gmail.com>
2022-07-06 17:29:52 -0700

    finished basic features

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,16 +1,16 @@
 const FeedbackData = [
     {
-      id: 1,
+      id: '1',
       rating: 8,
       text: 'This app is amazing!',
     },
     {
-      id: 2,
+      id: '2',
       rating: 8,
       text: 'Wow! Well done dude!',
     },
     {
-      id: 3,
+      id: '3',
       rating: 1,
       text: 'Bad.',
     },
